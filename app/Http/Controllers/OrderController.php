19ad210cfe05d9441809fb9d41e7caf2d8a348f8
tusharshahsp <tusharshahsp@gmail.com>
2018-08-09 14:09:11 +0530

    local commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,6 +10,13 @@ use Request;
 class OrderController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    
     public function showplaceorder(Request $request){
     	$order_id = Request::get('order_id');
     	return view('orders.order_details',['order' => Order::find($order_id)]);
