cff89905b3e459d234a0d98aff7f932241f9ee95
tusharshahsp <tusharshahsp@gmail.com>
2018-08-13 13:27:44 +0530

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,8 +28,10 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
-
+    protected function redirectTo()
+    {
+        return '/products';
+    }
     /**
      * Create a new controller instance.
      *
